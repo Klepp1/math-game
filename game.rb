bf67d9fe9836c8bf8a8a52644797bbc394fe37fd
Kyler <kylerlepp8@gmail.com>
2022-08-23 12:33:13 -0700

    created game file to update scores and change players and report scores

--- game.rb
+++ game.rb
@@ -1,1 +1,34 @@
+require './player'
 
+class Game
+
+  attr_reader :report_life, :players, :current_player
+### Initalize the players
+  def initialize (p1, p2)
+    @players = [p1, p2]
+    @current_index = 0
+    @current_player = @players[@current_index]
+  end
+
+  ## subtract lives method
+  def subtract_life
+    @current_player.life -= 1
+    if @current_player.life === 0
+      puts "#{@current_player.name} is out of lives boohoo you lose"
+      puts "--- GAME OVER ---"
+    end
+  end
+
+  ## reports the lives to console
+  def report_life
+    puts "life Remaining ==> P1: #{@players[0].life}/3 P2:#{@players[1].life}/3"
+  end
+  ## changes players
+  def change_player
+    if @current_player == @players[0]
+      @current_player = @players[1]
+    else
+      @current_player = @players[0]
+    end
+  end
+end
